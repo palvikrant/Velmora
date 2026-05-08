fd242878860af8487f7828f6f58b42f24883189a
Vikrant Pal <vikrantgadariya303@gmail.com>
2026-05-08 23:37:18 +0530

    Add files via upload

---
+++ script3.js
@@ -0,0 +1,12 @@
+const text = "Welcome to Velmora , explore more for our Blog page..!";
+let index = 0;
+
+function typeEffect() {
+    if (index < text.length) {
+        document.getElementById("animatedText").innerHTML += text.charAt(index);
+        index++;
+        setTimeout(typeEffect, 50);
+    }
+}
+
+window.onload = typeEffect;
